c9febdee7d4dd9db2455395eb8c7ff14677e7aa2
Shujaat Azim <shujaat.azim@gmail.com>
2022-02-05 12:00:56 -1100

    Added clarifying comments.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+// the colon is used to separate the "thing" from what the "thing" is defined as (in this case, a react component)
+
 const App: React.FC = () => {
   return (
     <div className="App">
